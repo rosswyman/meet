2e86d76085a15feb2b1f5bc514fbabe660358abd
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-08-09 20:46:35 -0500

    Update EventList.js
    Passed numberOfEvents state as props from app.  Truncate events per number of events.

--- src/EventList.js
+++ src/EventList.js
@@ -6,9 +6,12 @@ import Event from './Event';
 class EventList extends Component {
 	render() {
 		const { events } = this.props;
+		const { numberOfEvents } = this.props;
+		const limitedNumberOfEvents = events.slice(0, numberOfEvents);
+
 		return (
 			<ul className="EventList">
-				{events.map((event) => (
+				{limitedNumberOfEvents.map((event) => (
 					<li key={event.id}>
 						<Event event={event} />
 					</li>
