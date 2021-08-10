4827bb55c4e18ad8d58cd619ab9731f7f9cc983b
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-08-09 20:42:52 -0500

    Update mock-data.js
    Added additional events

--- src/mock-data.js
+++ src/mock-data.js
@@ -505,6 +505,486 @@ var mockData = [
 		},
 		eventType: 'default',
 	},
+
+	{
+		kind: 'calendar#event',
+		etag: '"3187237556010000"',
+		id: '111vmlbl7p8gmc0fbf5u107p8u_20200713T070000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=MTExdm1sYmw3cDhnbWMwZmJmNXUxMDdwOHVfMjAyMDA3MTNUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T15:47:59.000Z',
+		updated: '2020-07-01T15:52:58.005Z',
+		summary: 'React Native Tokyo',
+		description:
+			"We meet every Monday and Thursday to talk about React Native and native app development in general. React Native Tokyo is an inclusive community that tries to cater to all levels of learners during meetups. \n\nJoin us if you are curious to hear about what's new in the field of native app development.",
+		location: 'Tokyo, Japan',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-13T09:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-13T10:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '111vmlbl7p8gmc0fbf5u107p8u',
+		originalStartTime: {
+			dateTime: '2020-07-13T09:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '111vmlbl7p8gmc0fbf5u107p8u@google.com',
+		sequence: 1,
+		hangoutLink: 'https://meet.google.com/ceo-behe-ssp',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/ceo-behe-ssp',
+					label: 'meet.google.com/ceo-behe-ssp',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/ceo-behe-ssp?pin=2641490783465',
+					pin: '2641490783465',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-40-8081617770',
+					label: '+49 40 8081617770',
+					pin: '889553795',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'ceo-behe-ssp',
+			signature: 'ADXwMqMyo2gAgqEeJsGcG0OLXmRG',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3181159875584000"',
+		id: '3qtd6uscq4tsi6gc7nmmtpqlct_20200713T120000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=M3F0ZDZ1c2NxNHRzaTZnYzdubW10cHFsY3RfMjAyMDA3MTNUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-05-19T19:14:30.000Z',
+		updated: '2020-05-27T11:45:37.792Z',
+		summary: 'React is Fun',
+		description:
+			'Love HTML, CSS, and JS? Want to become a cool front-end developer? \n\nReact is one of the most popular front-end frameworks. There is a huge number of job openings for React developers in most cities. \n\nJoin us in our free React training sessions and give your career a new direction. ',
+		location: 'Berlin, Germany',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-13T14:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-13T15:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '3qtd6uscq4tsi6gc7nmmtpqlct',
+		originalStartTime: {
+			dateTime: '2020-07-13T14:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '3qtd6uscq4tsi6gc7nmmtpqlct@google.com',
+		sequence: 0,
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3187234500432000"',
+		id: '65jg7h2f96klim15fie75mj1a1_20200713T140000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=NjVqZzdoMmY5NmtsaW0xNWZpZTc1bWoxYTFfMjAyMDA3MTNUMTQwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T15:26:57.000Z',
+		updated: '2020-07-01T15:27:30.216Z',
+		summary: 'AngularJS Workshop',
+		description:
+			'Developed by Google, AngularJS is a relatively new JavaScript, and it is designed to make front-end development as easy as possible for you. \n\nJoin us to get introduced to this wonderful framework and dive deep into its features.',
+		location: 'Cape Town, South Africa',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-13T16:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-13T17:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '65jg7h2f96klim15fie75mj1a1',
+		originalStartTime: {
+			dateTime: '2020-07-13T16:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '65jg7h2f96klim15fie75mj1a1@google.com',
+		sequence: 1,
+		hangoutLink: 'https://meet.google.com/ctp-pwtc-pmq',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/ctp-pwtc-pmq',
+					label: 'meet.google.com/ctp-pwtc-pmq',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/ctp-pwtc-pmq?pin=2824243883315',
+					pin: '2824243883315',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-30-300195220',
+					label: '+49 30 300195220',
+					pin: '987090091',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'ctp-pwtc-pmq',
+			signature: 'ADXwMqO87SNP1Ybhv07wkETEW/cR',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3187224991890000"',
+		id: '3glt4dve9uip7oh5g5kb2bid2s_20200713T160000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=M2dsdDRkdmU5dWlwN29oNWc1a2IyYmlkMnNfMjAyMDA3MTNUMTYwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T14:07:59.000Z',
+		updated: '2020-07-01T14:08:15.945Z',
+		summary: 'Fun with Node.js',
+		description:
+			'Do you know that Node.js is basically a server having the capability of executing JavaScript? If you are familiar with JS, Node.js will be an easy ride. \n\nJoin us if you want to learn how to build the server-side of an app using Node.js.',
+		location: 'Nairobi, Kenya',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-13T18:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-13T19:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '3glt4dve9uip7oh5g5kb2bid2s',
+		originalStartTime: {
+			dateTime: '2020-07-13T18:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '3glt4dve9uip7oh5g5kb2bid2s@google.com',
+		sequence: 1,
+		hangoutLink: 'https://meet.google.com/wkx-akyy-omd',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/wkx-akyy-omd',
+					label: 'meet.google.com/wkx-akyy-omd',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/wkx-akyy-omd?pin=9536747460380',
+					pin: '9536747460380',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-40-8081616512',
+					label: '+49 40 8081616512',
+					pin: '576339034',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'wkx-akyy-omd',
+			signature: 'ADXwMqOAKn7ZT8sCEEBbgJD9ObYS',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3187221858184000"',
+		id: '3gi3kabkm3bua5lq3jhscc8s4c_20200713T200000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=M2dpM2thYmttM2J1YTVscTNqaHNjYzhzNGNfMjAyMDA3MTNUMjAwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T13:23:24.000Z',
+		updated: '2020-07-01T13:42:09.092Z',
+		summary: 'Intro to AngularJS-Remote',
+		description:
+			'Developed by Google, AngularJS is a relatively new JavaScript, and it is designed to make front-end development as easy as possible for you. Join us to get introduced to this wonderful framework and dive deep into its features. ',
+		location: 'New York, NY, USA',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-13T22:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-13T23:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '3gi3kabkm3bua5lq3jhscc8s4c',
+		originalStartTime: {
+			dateTime: '2020-07-13T22:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '3gi3kabkm3bua5lq3jhscc8s4c@google.com',
+		sequence: 2,
+		hangoutLink: 'https://meet.google.com/xcb-hfjb-psv',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/xcb-hfjb-psv',
+					label: 'meet.google.com/xcb-hfjb-psv',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/xcb-hfjb-psv?pin=4442155852117',
+					pin: '4442155852117',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-40-8081617300',
+					label: '+49 40 8081617300',
+					pin: '657955248',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'xcb-hfjb-psv',
+			signature: 'ADXwMqPcUqTZ56R/P0UVT8btXClk',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3187221506816000"',
+		id: '48m09le389vijbqfs2mcr6i87q_20200714T070000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=NDhtMDlsZTM4OXZpamJxZnMybWNyNmk4N3FfMjAyMDA3MTRUMDcwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T13:39:01.000Z',
+		updated: '2020-07-01T13:39:13.408Z',
+		summary: 'Node Gang',
+		description:
+			"We meet every Tuesday to talk about Node or JavaScript in general. Node Gang is an inclusive community that tries to cater to all levels of learners during meetups. Join us if you are curious to hear about what's new in Node, patterns, interesting npm packages, and practices.",
+		location: 'Sydney NSW, Australia',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-14T09:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-14T10:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '48m09le389vijbqfs2mcr6i87q',
+		originalStartTime: {
+			dateTime: '2020-07-14T09:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '48m09le389vijbqfs2mcr6i87q@google.com',
+		sequence: 1,
+		hangoutLink: 'https://meet.google.com/wdc-wutt-tji',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/wdc-wutt-tji',
+					label: 'meet.google.com/wdc-wutt-tji',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/wdc-wutt-tji?pin=3796108774207',
+					pin: '3796108774207',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-40-8081618974',
+					label: '+49 40 8081618974',
+					pin: '170634768',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'wdc-wutt-tji',
+			signature: 'ADXwMqN9KZmGKqFySPYSoMLTG/sW',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
+	{
+		kind: 'calendar#event',
+		etag: '"3187232532588000"',
+		id: '20c51je3q99mnolr1rp06dt9jd_20200714T120000Z',
+		status: 'confirmed',
+		htmlLink:
+			'https://www.google.com/calendar/event?eid=MjBjNTFqZTNxOTltbm9scjFycDA2ZHQ5amRfMjAyMDA3MTRUMTIwMDAwWiBmdWxsc3RhY2t3ZWJkZXZAY2FyZWVyZm91bmRyeS5jb20',
+		created: '2020-07-01T14:17:12.000Z',
+		updated: '2020-07-01T15:11:06.294Z',
+		summary: 'Use jQuery, bring in interactivity easily ',
+		description:
+			'Do you know jQuery is used by around 70 percent of the 10 million most popular websites as of May 2019? \n\nThough many consider it dead after Angular and Express gained popularity, jQuery is still an important part of many websites. \n\nIn our workshop, we teach basic to advanced jQuery where you will also be able to build a simple app using it. If you are familiar with JS, join us to learn probably its most popular library.',
+		location: 'Mumbai, Maharashtra, India',
+		creator: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		organizer: {
+			email: 'fullstackwebdev@careerfoundry.com',
+			self: true,
+		},
+		start: {
+			dateTime: '2020-07-14T14:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		end: {
+			dateTime: '2020-07-14T15:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		recurringEventId: '20c51je3q99mnolr1rp06dt9jd',
+		originalStartTime: {
+			dateTime: '2020-07-14T14:00:00+02:00',
+			timeZone: 'Europe/Berlin',
+		},
+		iCalUID: '20c51je3q99mnolr1rp06dt9jd@google.com',
+		sequence: 1,
+		hangoutLink: 'https://meet.google.com/nns-htdr-ijk',
+		conferenceData: {
+			entryPoints: [
+				{
+					entryPointType: 'video',
+					uri: 'https://meet.google.com/nns-htdr-ijk',
+					label: 'meet.google.com/nns-htdr-ijk',
+				},
+				{
+					entryPointType: 'more',
+					uri: 'https://tel.meet/nns-htdr-ijk?pin=9302146320475',
+					pin: '9302146320475',
+				},
+				{
+					regionCode: 'DE',
+					entryPointType: 'phone',
+					uri: 'tel:+49-40-8081618430',
+					label: '+49 40 8081618430',
+					pin: '810644618',
+				},
+			],
+			conferenceSolution: {
+				key: {
+					type: 'hangoutsMeet',
+				},
+				name: 'Google Meet',
+				iconUri:
+					'https://fonts.gstatic.com/s/i/productlogos/meet_2020q4/v6/web-512dp/logo_meet_2020q4_color_2x_web_512dp.png',
+			},
+			conferenceId: 'nns-htdr-ijk',
+			signature: 'ADXwMqMX3Gl9/Xrp3jzhDcHEqe7B',
+		},
+		reminders: {
+			useDefault: true,
+		},
+		eventType: 'default',
+	},
 ];
 
 mockData = JSON.parse(JSON.stringify(mockData));
