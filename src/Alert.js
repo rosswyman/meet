5cb098e0c87a6c9708104e01e9ac76b9f0a03d93
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-08-17 13:21:52 -0500

    Create Alert.js

---
+++ src/Alert.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Alert extends Component {
+	constructor(props) {
+		super(props);
+		this.color = null;
+	}
+
+	getStyle = () => {
+		return {
+			color: this.color,
+			'font-size': this.size,
+			'font-style': this.style,
+		};
+	};
+
+	render() {
+		return (
+			<div className="Alert">
+				<p style={this.getStyle()}>{this.props.text}</p>
+			</div>
+		);
+	}
+}
+
+class InfoAlert extends Alert {
+	constructor(props) {
+		super(props);
+		this.color = 'blue';
+	}
+}
+
+class ErrorAlert extends Alert {
+	constructor(props) {
+		super(props);
+		this.color = 'red';
+		this.size = 'x-large';
+		this.style = 'italic';
+	}
+}
+
+export { InfoAlert };
+export { ErrorAlert };
