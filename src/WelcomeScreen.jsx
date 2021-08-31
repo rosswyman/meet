49c93911d7a2924936fe7570c7a451fe53a50ad4
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-08-30 20:00:00 -0500

    Added additional context to welcome screen

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -6,6 +6,16 @@ function WelcomeScreen(props) {
   (
     <div className="WelcomeScreen">
     <h1>Welcome to the Meet app</h1>
+    <h4>This is an app developed while taking the <a href="https://careerfoundry.com/">Career Foundry</a> Full-Stack Developer Course</h4>
+    <h4>It is a React based app built via test-driven development.  The only calendar accessed is one created for the Career Foundry curiculum.  No user calendars are accessed.</h4>
+    <div>
+    <img
+            class="react-icon"
+            src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
+            alt="React logo"
+          />
+    </div>
+    
     <h4>Log in to see upcoming events around the world for full-stack developers</h4>
     <div className="button_cont" align="center">
       <div class="google-btn">
